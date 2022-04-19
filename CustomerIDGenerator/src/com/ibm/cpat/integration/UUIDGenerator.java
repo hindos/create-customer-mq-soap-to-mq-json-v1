8e2de6edac813f338c38d05fb696794fb584a314
Henda Salhi <henda.salhi@ibm.com>
2022-04-19 07:39:22 +0100

    first commit

---
+++ CustomerIDGenerator/src/com/ibm/cpat/integration/UUIDGenerator.java
@@ -0,0 +1,12 @@
+package com.ibm.cpat.integration;
+
+import java.util.UUID;
+
+public class UUIDGenerator {
+
+
+	public static String sampleTransform() {
+		return UUID.randomUUID().toString();
+	}
+
+}
